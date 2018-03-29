2187fc6ccb777bd8fcc877f9d875f57f041d0c2c
Harshavardhan <harshavarshan.ta.1004@gmail.com>
2018-03-29 15:20:03 +0530

    Search page

--- src/main/java/com/qa/demo/pages/LoginPage.java
+++ src/main/java/com/qa/demo/pages/LoginPage.java
@@ -2,4 +2,14 @@ package com.qa.demo.pages;
 
 public class LoginPage {
 
+	public void login() {
+		System.out.println(" LoginPage");
+	}
+	
+	public void loginT() {
+		System.out.println(" TLoginPage");
+	}
+	
+	
 }
+
