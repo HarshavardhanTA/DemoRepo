827a65b4846a320f65ac58cf415b834a4c0891a7
Harshavardhan <harshavarshan.ta.1004@gmail.com>
2018-03-29 15:25:25 +0530

    rg

---
+++ src/main/java/com/qa/demo/pages/RegistrationPage.java
@@ -0,0 +1,17 @@
+package com.qa.demo.pages;
+
+public class RegistrationPage {
+	
+	public void login() {
+		System.out.println(" TLoginPage");
+	}
+	
+	public void loginT() {
+		System.out.println(" TLoginPage");
+	}
+	
+	public void loginTT() {
+		System.out.println(" TLoginPage");
+	}
+
+}
